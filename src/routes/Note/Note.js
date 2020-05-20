243f82f97375e2e64b9f8929c07a4b9ba643f9b4
DoLim <limdongyoung0@gmail.com>
2020-05-20 11:02:46 +0000

    feat: Notes route complete

--- src/routes/Note/Note.js
+++ src/routes/Note/Note.js
@@ -1,6 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Query } from "react-apollo";
 import { GET_NOTE } from "../../queries";
+import styled from "styled-components";
+import MarkdownRenderer from "react-markdown-renderer";
+
+const TitleComponent = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 50px;
+`;
+
+const Title = styled.h1`
+  font-size: 50px;
+  margin: 0;
+  padding: 0;
+`;
+
+const Button = styled.button``;
 
 export default (...props) => {
   const {
@@ -8,10 +26,34 @@ export default (...props) => {
       params: { id },
     },
   } = props[0];
-  console.log("id:", id);
+  // console.log("id:", id);
+  // const { loading, error, data } = useQuery(GET_NOTE, {
+  //   variables: { id },
+  // });
+  // console.log(
+  //   "From hooks!!!",
+  //   "loading:",
+  //   loading,
+  //   "error:",
+  //   error,
+  //   "data:",
+  //   data
+  // );
   return (
     <Query query={GET_NOTE} variables={{ id }}>
-      {(data) => console.log("data:", data)}
+      {({ data }) =>
+        data ? (
+          <>
+            <TitleComponent>
+              <Title>{data.note && data.note.title}</Title>
+              <Link to={`/edit/${data.note.id}`}>
+                <Button>Edit</Button>
+              </Link>
+            </TitleComponent>
+            <MarkdownRenderer markdown={data.note.content} />{" "}
+          </>
+        ) : null
+      }
     </Query>
   );
 };
