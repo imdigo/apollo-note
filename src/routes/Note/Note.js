a9f1f47f5b14e125142a4decf4d2335173373987
DoLim <limdongyoung0@gmail.com>
2020-05-20 10:33:19 +0000

    Not working

--- src/routes/Note/Note.js
+++ src/routes/Note/Note.js
@@ -1,3 +1,17 @@
-export default () => {
-  return "hi";
+import React from "react";
+import { Query } from "react-apollo";
+import { GET_NOTE } from "../../queries";
+
+export default (...props) => {
+  const {
+    match: {
+      params: { id },
+    },
+  } = props[0];
+  console.log("id:", id);
+  return (
+    <Query query={GET_NOTE} variables={{ id }}>
+      {(data) => console.log("data:", data)}
+    </Query>
+  );
 };
