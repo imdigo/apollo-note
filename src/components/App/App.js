ea9caf41769562a27c8b5648ce9861f86619e43c
DoLim <limdongyoung0@gmail.com>
2020-05-20 10:06:23 +0000

    feat: Notes Route

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Query } from "react-apollo";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Notes from "../../routes/Notes";
 import Note from "../../routes/Note/Note";
